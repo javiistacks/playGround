1625fc991acecfd3bc6ad6ac1008957241f2a084
Javiistacks <javiann.marrero@icloud.com>
2022-01-10 15:27:19 -0600

    while loops

--- forLoop.js
+++ forLoop.js
@@ -15,4 +15,73 @@ for (let counter = 5; counter < 11; counter++) {
   }
   //prints numbers 10 to zero. 
 
-  
+
+  const vacationSpots = ['Bali', 'Paris', 'Tulum'];
+   //Write a for loop that iterates through
+  // our vacationSpots array using i as the iterator variable.
+  for (let i = 0; i < vacationSpots.length; i++) {
+    console.log('I would love to visit'+ vacationSpots[i]);
+  }
+//prints 
+// I would love to visit Bali
+// I would love to visit Paris
+// I would love to visit Tulum
+
+//nested loop
+// Write your code below
+let bobsFollowers = ['Joe', 'Marta', 'Sam', 'Erin'];
+let tinasFollowers = ['Sam', 'Marta', 'Elle'];
+let mutualFollowers = [];
+
+for (let i = 0; i < bobsFollowers.length; i++) {
+  for (let j = 0; j < tinasFollowers.length; j++) {
+    if (bobsFollowers[i] === tinasFollowers[j]) {
+      mutualFollowers.push(bobsFollowers[i]);
+    }
+  }
+};
+
+
+
+const cards = ['diamond', 'spade', 'heart', 'club'];
+
+// Write your code below
+let currentCard;
+
+while ( currentCard != 'spade') {
+  currentCard = cards[Math.floor(Math.random() * 4)];
+	console.log(currentCard);
+}
+
+// do...while statements
+/*In some cases, you want a piece of code to run at 
+least once and then loop based on a 
+specific condition after its initial run. 
+This is where the do...while statement comes i 
+ */
+
+let cupsOfSugarNeeded = 3;
+let cupsAdded = 0;
+
+do {
+ cupsAdded++
+ console.log(cupsAdded + ' cup was added') 
+} while (cupsAdded < cupsOfSugarNeeded);
+//this prints out 
+//1 cup of sugar added
+//2 cup of sugar added
+//3 cup of sugar added
+
+//below is an example of the while loop.
+/*
+the code below will print out all the arrays until spade is called
+*/
+const cards = ['diamond', 'spade', 'heart', 'club'];
+
+// Write your code below
+let currentCard;
+
+while ( currentCard != 'spade') {
+  currentCard = cards[Math.floor(Math.random() * 4)];
+	console.log(currentCard);
+}
